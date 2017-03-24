2948c12476fcd8cc62a6e714fdd3e48dbb697409
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2017-03-24 11:04:44 +0100

    Fix citus_relation_size

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # citus_functions
 Some SQL functions for citus
+
+
+## citus_relation_size(regclass)
+
+Disk space usage by all shard of the specified table or index,
+equivalent to `pg_relation_size()`
+
+### Usage
+
+On the primary node type
+
+`db$ SELECT citus_relation_size('foobar');`
---
+++ citus_relation_size.sql
@@ -0,0 +1,63 @@
+BEGIN;
+    
+DROP FUNCTION IF EXISTS citus_relation_size(regclass);
+
+
+CREATE FUNCTION citus_relation_size(rel regclass) RETURNS text AS $BODY$
+DECLARE
+    nodes RECORD;
+    workers integer;
+    connstr text;
+    cmd text;
+    global_result boolean;
+    relsize bigint;
+    cursize bigint;
+    text_var1 text;
+    text_var2 text;
+    text_var3 text;
+BEGIN
+    global_result := true;
+    relsize := 0;
+    
+    FOR nodes IN
+
+        SELECT distinct on ( sh.shardid) sh.shardid::text,  nodename, nodeport
+        FROM pg_dist_shard sh
+        JOIN pg_dist_shard_placement sp ON sh.shardid = sp.shardid 
+        WHERE logicalrelid = rel
+    
+    LOOP
+        connstr := 'dbname=' || current_database() ||' host=' || nodes.nodename || ' port=' || nodes.nodeport;
+
+        cmd := 'SELECT pg_relation_size(' || quote_literal (rel::text || '_' || nodes.shardid) || ')';
+    
+
+        BEGIN
+        SELECT * FROM dblink(connstr, cmd )
+        AS t1(relsize bigint)
+        INTO cursize;
+
+        relsize := relsize + cursize;
+    
+        EXCEPTION WHEN OTHERS THEN
+             GET STACKED DIAGNOSTICS text_var1 = MESSAGE_TEXT,
+                                     text_var2 = PG_EXCEPTION_DETAIL,
+                                     text_var3 = PG_EXCEPTION_HINT;
+
+             global_result := false;
+        END;
+
+        BEGIN
+        -- finally close the link
+        PERFORM (dblink_disconnect('citus_tool'));
+            EXCEPTION WHEN OTHERS THEN
+                global_result := false;
+        END;
+    END LOOP;
+
+    RETURN relsize;
+
+END;
+$BODY$ LANGUAGE plpgsql;
+    
+COMMIT;
